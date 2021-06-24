21c4c9574983453dd534e5783af141508939b3d5
Aiam Abadilla <48824639+aiamabadilla@users.noreply.github.com>
2021-06-24 12:29:53 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const toggleButton = document.querySelector(".toggle");
 const navbarLinks = document.querySelector(".navbar");
 const navLinks = document.querySelectorAll(".navlink");
-const formSubmit = document.querySelector(".form__submit");
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
